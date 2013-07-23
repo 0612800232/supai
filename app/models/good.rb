131227c5cbcb9c318c473de14db1d7c2a99f6d63
unknown <lee@lee-PC.(none)>
2013-07-23 18:00:29 +0800

    daf

--- app/models/good.rb
+++ app/models/good.rb
@@ -1,10 +1,12 @@
 class Good < ActiveRecord::Base
   belongs_to :goods_type
   validates_presence_of :name,:message => "产品名称不为空"
-   validates_presence_of :desc,:message => "产品描述不为空"
+   validates_presence_of :desc,:message => "产品口味特色不为空"
   validates_uniqueness_of :name,:message => "产品名称已经被使用"
-  validates_presence_of :amount,:message => "产品金额不为空"
-  validates_format_of :amount, :with => /^(([1-9](\d{0,12}))|([0]{1}))(\.(\d){1,2})?$/,
-    :message => "请输入正确的金额"
+  validates_presence_of :weight,:message => "产品重量不为空"
+  validates_presence_of :makeing,:message => "产品制作方法不为空"
+  validates_presence_of :cuisines,:message => "产品菜系不为空"
+  validates_presence_of :burden,:message => "产品配料不为空"
+   validates_presence_of :s_d,:message => "菜品特色不为空"
 end
 
